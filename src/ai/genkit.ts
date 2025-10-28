9f4ac6163862835ce01932714cf78fec9c268f52
Fortune Domination <fortunedomination@gmail.com>
2025-10-28 12:55:47 +0000

    AI Summary Failed Failed to fetch from https://generativelanguage.google

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,7 +1,8 @@
+
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/google-genai';
 
 export const ai = genkit({
-  plugins: [googleAI()],
+  plugins: [googleAI({apiKey: 'AIzaSyDpoJ3h889m_6bhj1k0DmgmxWF3d9vupLw'})],
   model: 'googleai/gemini-2.5-flash',
 });
