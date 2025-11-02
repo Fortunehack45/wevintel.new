42ffb82cf1c2c7e1d8c2881037db77723c8d67d9
Fortunehack45 <151121333+Fortunehack45@users.noreply.github.com>
2025-11-02 08:19:45 +0100

    Remove apiKey from Firebase config
    Removed the apiKey value from Firebase configuration.

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -6,7 +6,7 @@ import { initializeApp, getApps } from "firebase/app";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBkBUrt-28Mq0ggKJh7JAX7qTKzi9dRtf4",
+  apiKey:,
   authDomain: "finai-rv8zz.firebaseapp.com",
   databaseURL: "https://finai-rv8zz-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "finai-rv8zz",
